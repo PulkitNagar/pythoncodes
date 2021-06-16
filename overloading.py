7dbfc6eebbc8dd765c22fb79a3ffd2f1f51040e0
Ansh Gupta <iamtheanshgupta@gmail.com>
2021-06-16 11:37:59 +0530

    .

--- overloading.py
+++ overloading.py
@@ -1,6 +1,12 @@
 class person:
-    def call(self,name = "none" ,age = "none"):
-        if (name is not "none" and age is not "none"):
-            print("hellow"+name+"age is "+age)
-        elif(name is not "none" and age is "none"):
-            print("hellow"+name)
+    def call(self,name = "none" ,age = 0):
+        if (name is not "none" and age != 0):
+            print("hellow "+name)
+            print("my age is ",age)
+        elif(name is not "none" and age == 0):
+            print("hellow "+name)
+        else:
+            print("hellow")
+
+ob=person()
+ob.call("ansh")
