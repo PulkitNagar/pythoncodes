7a1fac4a82f0d56b18e3fdc422f2af7a742ede33
Ansh Gupta <iamtheanshgupta@gmail.com>
2021-06-16 11:33:47 +0530

    .

--- magicmethods.py
+++ magicmethods.py
@@ -1,22 +1,26 @@
 from magic import *
+
 ob = magic()
 ob1 = magic()
+
 ob.input()
 ob1.input()
 
-
 while True:
-    print("1-add \n 2-sub \n 3-mul \n 4-div \n 0-exit")
-    ch = int(input("enter your choice"))
-    if ch == 1:
-        ob+ob1
-    elif ch == 2:
-        ob-ob1
-    elif ch == 3:
-        ob*ob1
-    elif ch == 4:
-        ob/ob1
-    elif ch == 0:
-        break
-    else:
-        break
+        print("1- add \n ")
+        print("2- sub \n ")
+        print("3- mul \n ")
+        print("4- truediv \n ")
+        print("0- exit \n ")
+
+        ch = int(input('Enter your choice : '))
+        if ch == 1:
+            ob+ob1
+        elif ch==2:
+            ob-ob1
+        elif ch==3:
+            ob*ob1
+        elif ch==4:
+            ob/ob1
+        else:
+            break
--- overloading.py
+++ overloading.py
@@ -1,3 +1,6 @@
 class person:
-    def call(self,name=none,age=0):
-        if name=="none" 
+    def call(self,name = "none" ,age = "none"):
+        if (name is not "none" and age is not "none"):
+            print("hellow"+name+"age is "+age)
+        elif(name is not "none" and age is "none"):
+            print("hellow"+name)
